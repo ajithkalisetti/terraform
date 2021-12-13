35c184dcb88756301bc2a0ae03216b6d0857ebeb
ajithkalisetti <83327765+ajithkalisetti@users.noreply.github.com>
2021-12-13 17:26:40 +0530

    Add files via upload

---
+++ var.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+variable "ip" {
+type = string
+  cidr_block = "190.10.0.0/20"
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "190.10.0.0/16"
+
+
+  tags = {
+    Name = 'Thala'
+  }
+}
+resource "aws_subnet" "subnet1" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = var.ip
+
+
+  tags = {
+    Name = "subnet1"
+  }
+}
