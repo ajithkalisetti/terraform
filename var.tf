b07e6c0b40bb1221b3418ea0cc10afa6e82ee2ae
ajith kalisetti <123vamsisaiajith@gmail.com>
2023-05-13 18:24:35 +0530

    modified var.tf

---
+++ var.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the resource group"
+}
+
+variable "resource_group_location" {
+  type        = string
+  description = "Location of the resource group"
+}
+
+variable "vnet_name" {
+  type        = string
+  description = "Name of the VNet"
+}
+
+variable "vnet_location" {
+  type        = string
+  description = "Location of the VNet"
+}
